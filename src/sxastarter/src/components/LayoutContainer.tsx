411bf5f78422e205aabec1424d412b4c2e35e408
Stanislav Maxymov <stanislav.maxymov@sitecore.com>
2024-01-29 11:08:21 +0200

    Update component and serialization

--- src/sxastarter/src/components/LayoutContainer.tsx
+++ src/sxastarter/src/components/LayoutContainer.tsx
@@ -10,42 +10,70 @@ interface ComponentProps {
   params: ComponentParams;
 }
 
+export function parseInlineStyles(styles: string | undefined): Record<string,string> {
+  const stylePairs = styles?.split(';') ?? [];
+  const styleObject: Record<string,string> = {};
+  stylePairs.forEach((pair) => {
+    const [key, value] = pair.split(':');
+    if(key && value){
+      styleObject[key.trim()] = value.trim();
+    }
+  });
+
+  return styleObject;
+}
+
+
 export const Default = (props: ComponentProps): JSX.Element => {
 
-  const columnClasses = [
-    props.params.Column1Classes,
-    props.params.Column2Classes,
-    props.params.Column3Classes,
-    props.params.Column4Classes,
-    props.params.Column5Classes,
-    props.params.Column6Classes,
-    props.params.Column7Classes,
-    props.params.Column8Classes,
+  const columnSizes = [
+    props.params.ColumnSize1,
+    props.params.ColumnSize2,
+    props.params.ColumnSize3,
+    props.params.ColumnSize4,
+    props.params.ColumnSize5,
+    props.params.ColumnSize6,
+    props.params.ColumnSize7,
+    props.params.ColumnSize8,
   ];
 
-  const styles = JSON.parse(props.params.ContainerStyles ?? "{}") as Record<string, any>;
-	const columnStyles = JSON.parse(props.params.ColumnStyles ?? "[]") as Array<Record<string, any>>;
+  const columnStyles = [
+    props.params.ColumnStyle1,
+    props.params.ColumnStyle2,
+    props.params.ColumnStyle3,
+    props.params.ColumnStyle4,
+    props.params.ColumnStyle5,
+    props.params.ColumnStyle6,
+    props.params.ColumnStyle7,
+    props.params.ColumnStyle8,
+  ];
 
-
+  const containerStyles =  parseInlineStyles(props.params.ContainerStyles);
   let enabledColumns = Number.parseInt(props.params.EnabledColumns);
-  if (isNaN(enabledColumns)) {
+
+  if (isNaN(enabledColumns)|| enabledColumns < 1) {
     enabledColumns = 1;
   }
+
+  if(enabledColumns === 1 && !columnSizes[0]){
+    columnSizes[0] = 'basis-full';
+  }
+
   const enabledColIndexes = [...Array(enabledColumns).keys()];
 
   const id = props.params.RenderingIdentifier;
-
+  
   return (
     <div style={{width: '100%'}}>
       <div
-        style={styles}
+        style={containerStyles}
         className={`layout-container`}
         id={id ? id : undefined}
       >
         {enabledColIndexes.map((index) => {
-          const phKey = `column-${index + 1}-{*}`;
-          const columnClass = columnClasses[index];
-					const columnStyle = columnStyles[index];
+          const phKey = `layout-column-${index + 1}-{*}`;
+          const columnClass = columnSizes[index] ?? '';
+          const columnStyle = columnStyles[index] ?? '';
 
           return (
             <div key={index + 1} className={`${columnClass} layout-column`}>
